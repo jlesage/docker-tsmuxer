978f60f6793d8eeb97edc977bf4e4f395605b11a
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-07-23 07:06:13 -0400

    Adjusted shebang.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/with-contenv sh
 # NOTE: The $HOME variable is set only to have a default location when opening
 #       the file dialog window.
 exec env QT_X11_NO_MITSHM=1 HOME="/storage" /opt/tsmuxer/tsMuxerGUI
