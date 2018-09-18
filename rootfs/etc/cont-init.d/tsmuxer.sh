79d9c98cadc27431d6c7e5def6c28e83411d1c8c
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-09-18 13:45:05 -0400

    Make sure to set ownership on all files/folders, event hidden ones.

--- rootfs/etc/cont-init.d/tsmuxer.sh
+++ rootfs/etc/cont-init.d/tsmuxer.sh
@@ -14,6 +14,6 @@ mkdir -p "$XDG_CONFIG_HOME/Network Optix"
 [ -f /config/Trolltech.conf ] || cp -v /defaults/Trolltech.conf "$XDG_CONFIG_HOME/"
 
 # Take ownership of the config directory content.
-chown -R $USER_ID:$GROUP_ID /config/*
+find /config -mindepth 1 -exec chown $USER_ID:$GROUP_ID {} \;
 
 # vim: set ft=sh :
