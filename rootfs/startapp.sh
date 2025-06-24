4ae41a1539e65568ce76c4d64b63a4381be5290c
Jocelyn Le Sage <jocelyn@le-sage.com>
2025-06-23 21:38:04 -0400

    Prevent error message in log about missing swrast_dri.so.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 # NOTE: The $HOME variable is set only to have a default location when opening
 #       the file dialog window.
-#exec env QT_X11_NO_MITSHM=1 HOME="/storage" /opt/tsmuxer/tsMuxerGUI
-exec env HOME="/storage" /usr/bin/tsMuxerGUI
+export HOME=/storage
+
+# Added to avoid the following error message:
+#   MESA-LOADER: failed to open swrast: Error loading shared library
+#   /usr/lib/xorg/modules/dri/swrast_dri.so: No such file or directory
+#   (search paths /usr/lib/xorg/modules/dri, suffix _dri)
+# We could instead install `mesa-dri-gallium`, but this increases the image
+# size a lot.
+export QT_QPA_PLATFORM=xcb
+export QT_XCB_GL_INTEGRATION=none
+
+exec /usr/bin/tsMuxerGUI
 # vim:ft=sh:ts=4:sw=4:et:sts=4
